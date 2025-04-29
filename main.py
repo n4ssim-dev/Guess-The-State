1c671c9fc0b6de2f5573f91e6c67595d18b34a7f
Nassim <achnassim34@gmail.com>
2025-04-29 18:19:47 +0200

    Doc

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import pandas
 
 score = 0
 
+# Importe le fichier des états
 file = pandas.read_csv('50_states.csv')
 file_us_state = list(file.state)
 
+# Créer un dictionnary avec un état associé à une coordonnée
 us_states = {}
 
 for index, row in file.iterrows():
     us_states[row["state"]] = (row["x"], row["y"])
 
+# Création de l'écran
 screen = turtle.Screen()
 screen.title("Guess The States")
 image = 'blank_states_img.gif'
@@ -31,14 +34,14 @@ def give_question():
             prompt="Guess the name of a U.S. State :"
         )
 
-        if not guessed_state:  # Handle case when user closes the prompt
+        if not guessed_state:
             break
 
         guessed_state = guessed_state.title()
 
+        # coupe le jeu
         if guessed_state == "Exit":
             print("You stopped ...")
-            # Calculate states not guessed
             non_guessed_states = [state for state in file_us_state if state not in guessed_states]
             print("States you missed:", non_guessed_states)
 
@@ -46,6 +49,7 @@ def give_question():
             pandas.DataFrame(non_guessed_states, columns=["state"]).to_csv("states_to_learn.csv")
             break
 
+        # Ajoute un état deviné à la carte
         if guessed_state in us_states and guessed_state not in guessed_states:
             t = turtle.Turtle()
             t.penup()
